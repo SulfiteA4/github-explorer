d9b65441d2ad1ef259d91119f62fe020590962b3
SulfiteA4 <lhungaro10@gmail.com>
2022-10-23 09:54:24 -0300

    configuração e estruturação  inicial do projeto

---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import { render } from 'react-dom';
+import { App } from './App'
+ 
+render(<App />, document.getElementById('root')) //recebe dois parametros: o 1º e o que eu quero renderizar ou exibir em tela e o 2º e dentro do qual elemento ele será renderizado
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path'); //export feito pelo node 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),//acessa o diretorio que esta o arquivo principal do projeto - utilizando as convenções de cada SO em relação a escrita de caminhos de diretório.Parametros: __dirname = Diretorio do arquivo que esta o webpack.config.js; e o restante são as pastas até o arquivo.
+    output: {//fala qual o arquivo que sera geerado com o webpack, que é o bundle.js.
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'//nesse caso, o nome do arquivo é um atributo separado do path.
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],//linha que determina os tipos de extensões que webpaxk pode ler.
+    },
+    devServer: {
+        watchFiles: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
+    module: {//determina como a aplicação se comporta nas importações pra cada tipo de arquivo.
+        rules: [
+            {
+                test: /\.jsx$/,//teste do tipo de arquivo que será importado.
+                exclude: /node_modules/,//exclui arquivos que já estão prontos para o browser ler se o test retornar.
+                use: 'babel-loader',//Basicamente, o babel-loader é a integração entre o babel e o webpack.
+
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ],
+    }
+}
