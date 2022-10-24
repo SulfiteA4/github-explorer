2add58436cc45dd3e09761f5879faf0d83eefd3e
SulfiteA4 <lhungaro10@gmail.com>
2022-10-23 21:23:11 -0300

    add useEffect e useState e fazendo a chamada da api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -8,7 +11,12 @@ const repository = {
 }
 
 export function RepositoryList() {
-
+    const [repositories, setRepositories] = useState([]);// sempre que for um listagem deve-se começar o estado com um array vazio, além disso, é uma boa convenção iniciar o estado de uma variavel com o mesmo tipo de informação que vai ser armazenada.
+    useEffect(()=>{
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, [])// parametros: qual funcao vai ser disparada; quando ela vai ser disparada. Se for passado um array vazio a funcao vai ser executada somente uma vez, caso nao for passado nenhum parametro sera executada infinitas vezes.
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
